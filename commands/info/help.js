a09fa958b62488b4541ee067960bba980d37658a
Purge <77882159+Purge0827@users.noreply.github.com>
2021-02-22 20:52:43 -0800

    Add files via upload

---
+++ commands/info/help.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js")
+module.exports = {
+  name: "help",
+  aliases: ["halp"],
+  clientPerms: ["EMBED_LINKS"],
+  run: async (client, message, args) => {
+    const embed = new Discord.MessageEmbed()
+    .setTitle("Help Center")
+    .setDescription("**Here are list of my commands**")
+    .setThumbnail(client.user.displayAvatarURL({ format: "png" }))
+      .addField(`Moderation`, '`kick` | `ban` | `slowmode` | `unban` | `nuke` | `lock` | `unlock`')
+      .addField(`Information`, '`serverinfo`')
+        .addField(`utility`, ' `ping` | `purge` | `avatar` | `snipe` | `steal` | `covid`')
+        .addField(`Fun`, '`meme` | `8ball` | `say`')
+    .setColor("RANDOM")
+    .setTimestamp()
+    .setFooter(`Made by Purge#1338`, client.user.displayAvatarURL({ format: "png" }))
+    message.channel.send(embed)
+  }
+}
